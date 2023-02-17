de228d18e2f34fbcdf15ddba7398669304d1f98b
victorygit <victor.yang@opg.com>
2023-02-17 08:05:14 -0500

    add pipeline3

--- adf_pipelin3.py
+++ adf_pipelin3.py
@@ -27,7 +27,7 @@ default_args = {
 
 # Create the DAG
 dag = DAG(
-    'run_pipeline12',
+    'run_pipeline3',
     default_args=default_args,
     schedule_interval=timedelta(days=1),
 )
@@ -53,7 +53,7 @@ def run_pipeline1(**kwargs):
     #client = ResourceManagementClient(credentials, 'a4019287-f428-40ff-8fb5-3224e84aeb1e')
 
     # Run the pipeline
-    pipeline_name = 'PL_Job1'
+    pipeline_name = 'PL_Job3'
     run_response = client.pipelines.create_run(
     'oceanis-rg-dld-sb',
     'oceanis-adf-dldtest-sb',
@@ -74,61 +74,17 @@ def run_pipeline1(**kwargs):
         raise AirflowFailException("Pipeline failed")
     Variable.set('curated_log',Variable.get('curated_log')+',pipeline1')
     Job_Dependency_dict = Variable.get("Job_Dependency",deserialize_json=True)
-    log_str = Job_Dependency_dict["Log"] +',pipeline1'
-    Job_Dependency_dict["Log"] = log_str
-    Variable.set('Job_Dependency',json.dumps(Job_Dependency_dict), serialize_json= False)
-
-def run_pipeline2(**kwargs):
-    # Create the client
-    credentials = ClientSecretCredential(
-        client_id='12c29c27-283d-46c8-9b3a-1515836cf62a',
-        client_secret = Variable.get("Client_Secret"),
-        tenant_id='962f21cf-93ea-449f-99bf-402e2b2987b2',
-    )
-    
-    client = DataFactoryManagementClient(credentials, 'a4019287-f428-40ff-8fb5-3224e84aeb1e')
-
-    # Run the pipeline
-    pipeline_name = 'PL_Job2'
-    run_response = client.pipelines.create_run(
-    'oceanis-rg-dld-sb',
-    'oceanis-adf-dldtest-sb',
-    pipeline_name,
-    None,
-    None,
-    None,
-    None,
-    {"Waitime":kwargs['waittime'],"Job":kwargs['job']}
-    )
-    run_id = run_response.run_id
-    # Print the run ID
-    print(f'Pipeline run ID: {run_id}')
-
-    while client.pipeline_runs.get('oceanis-rg-dld-sb', 'oceanis-adf-dldtest-sb', run_response.run_id).status in ('InProgress', 'Queued'):
-        time.sleep(20) 
-    if client.pipeline_runs.get('oceanis-rg-dld-sb', 'oceanis-adf-dldtest-sb', run_response.run_id).status in ('Failed'):
-        raise AirflowFailException("Pipeline failed")
-    Variable.set('curated_log',Variable.get('curated_log')+',pipeline2')
-    Job_Dependency_dict = Variable.get("Job_Dependency",deserialize_json=True)
-    log_str = Job_Dependency_dict["Log"] +',pipeline2'
+    log_str = Job_Dependency_dict["Log"] +',pipeline3'
     Job_Dependency_dict["Log"] = log_str
     Variable.set('Job_Dependency',json.dumps(Job_Dependency_dict), serialize_json= False)
 
 # Create a PythonOperator to run the pipeline
 run_pipeline_operator1 = PythonOperator(
-     task_id='run_pipeline1',
+     task_id='run_pipeline3',
      python_callable=run_pipeline1,
      provide_context=True,
-     op_kwargs={"waittime":"15","job":"100"},
+     op_kwargs={"waittime":"15","job":"300"},
      dag=dag,
- )
-run_pipeline_operator2 = PythonOperator(
-     task_id='run_pipeline2',
-     python_callable=run_pipeline2,
-     provide_context=True,
-     op_kwargs={"waittime":"5","job":"200"},
-     dag=dag,
-     trigger_rule= 'all_success'
  )
 
 
@@ -137,4 +93,3 @@ sleep = BashOperator(task_id='sleep',
                      dag=dag)
 # Set the dependencies
 begin >> sleep >> run_pipeline_operator1 >> end
-sleep >> run_pipeline_operator2 >> end
