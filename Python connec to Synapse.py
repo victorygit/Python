f72e7b48d71b7cdbe4637992175a7d2f0957f301
victorygit <victor.yang@opg.com>
2023-02-25 10:35:51 -0500

    AD login SQL

--- Python connec to Synapse.py
+++ Python connec to Synapse.py
@@ -17,3 +17,26 @@ with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE=
             row = cursor.fetchone()
 df = pd.read_sql("SELECT * FROM [INFORMATION_SCHEMA].[COLUMNS]", conn)
 print(df.head(5))
+
+server = 'oceanis-syn-finance-dev-ondemand.sql.azuresynapse.net'
+database = 'synwssqlpool'
+username ='victor.yang@opg.com'
+Authentication='ActiveDirectoryInteractive'
+driverad= '{ODBC Driver 17 for SQL Server}'
+connad = pyodbc.connect('DRIVER='+driver+
+                      ';SERVER='+server+
+                      ';PORT=1433;DATABASE='+database+
+                      ';UID='+username+
+                      ';AUTHENTICATION='+Authentication
+                      )
+
+print(connad)
+with connad.cursor() as cursorad:
+    cursorad.execute("SELECT TOP 3 * FROM [INFORMATION_SCHEMA].[COLUMNS]")
+    rowad = cursorad.fetchone()
+    while rowad:
+    #print (str(row[0]) + " " + str(row[1])+ " " + str(row[2])+ " " + str(row[3]))
+        print(rowad)
+        rowad = cursorad.fetchone()
+df_ad = pd.read_sql("SELECT * FROM [INFORMATION_SCHEMA].[COLUMNS]", connad)
+print(df_ad.head(5))
