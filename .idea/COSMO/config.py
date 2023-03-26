c9a6ae2540cc4b9d1ca6efb5428a72095ee2df03
victorygit <victor.yang@opg.com>
2023-03-26 16:02:20 -0400

    remove key

--- .idea/COSMO/config.py
+++ .idea/COSMO/config.py
@@ -2,7 +2,7 @@ import os
 
 settings = {
     'host': os.environ.get('ACCOUNT_HOST', 'https://oceanis-cosmo-test.documents.azure.com:443/'),
-    'master_key': os.environ.get('ACCOUNT_KEY', 'jkgrgOb6UrrlOPDkZSrrAyZuy5sPadzejXGiIIwWbk5a3nlsCDKZ0PiPWQxLDuUzyKLzpDm4Qc4QACDbhTLpzQ=='),
+    'master_key': os.environ.get('ACCOUNT_KEY', 'jxx'),
     'database_id': os.environ.get('COSMOS_DATABASE', 'Employee'),
     'container_id': os.environ.get('COSMOS_CONTAINER', 'Items'),
 }
